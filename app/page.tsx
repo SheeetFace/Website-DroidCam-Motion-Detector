591b0386b7856ec1b952cb313465add4c7f1649b
Alex <sheeetface333@gmail.com>
2024-11-28 16:24:26 +0300

    FEAT: add VideoExample component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 
 import HeaderHome from "@/components/home/HeaderHome";
 import CardHome from "@/components/home/CardHome";
+import VideoExample from "@/components/home/VideoExample";
 import { siteConfig } from "@/config/site";
 
 export default function Home() {
@@ -27,11 +28,16 @@ export default function Home() {
         <HeaderHome />
         <motion.section
           animate={{ opacity: 1, y: 0 }}
-          className="flex flex-col gap-5 mt-8 md:flex-row"
+          className="mt-8"
           initial={{ opacity: 0, y: -30 }}
           transition={{ duration: 0.5, ease: "easeOut", delay: 0.8 }}
         >
-          {renderCards()}
+          <div className="flex flex-col md:flex-row justify-between w-full">
+            <div className="flex flex-col gap-5 md:flex-row">
+              {renderCards()}
+            </div>
+            <VideoExample />
+          </div>
         </motion.section>
       </div>
     </section>
---
+++ components/home/VideoExample.tsx
@@ -0,0 +1,43 @@
+export default function VideoExample() {
+  return (
+    <section
+      aria-label="This video showcases how the DroidCam motion detection system operates in real-world scenarios."
+      aria-labelledby="video-title"
+      className="w-[376px]"
+    >
+      <div className="relative w-full h-[280px] overflow-hidden rounded-t-xl">
+        <iframe
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+          className="absolute top-0 left-1/2 -translate-x-1/2"
+          height="280"
+          src="https://www.youtube.com/embed/qHWuTPer6aE?autoplay=1&controls=0&modestbranding=1&rel=0&showinfo=0&mute=1&loop=1&playlist=qHWuTPer6aE"
+          title="Demonstration of DroidCam Motion Detection Features"
+          width="500"
+        />
+        <div className="absolute inset-0 z-10 bg-transparent" />
+      </div>
+
+      <div className="bg-zinc-900 rounded-b-xl ps-2 py-2 relative z-50">
+        <h2 className="sr-only" id="video-title">
+          DroidCam Motion Detection Demonstration
+        </h2>
+        <p className="sr-only">
+          This video showcases how the DroidCam motion detection system operates
+          in real-world scenarios, highlighting its accuracy and efficiency.
+        </p>
+
+        <ul className="mt-1 text-xs text-gray-500 flex flex-col items-start ">
+          <li>
+            <strong>Version:</strong> 1.0.0
+          </li>
+          <li>
+            <strong>Updated:</strong> 30 November 2024
+          </li>
+          <li>
+            <strong>Author:</strong> SheeetFace
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}
