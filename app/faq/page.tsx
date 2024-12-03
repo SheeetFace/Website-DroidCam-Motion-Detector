a03572c1698fef48ecba87c66851fa049a179bbc
Alex <sheeetface333@gmail.com>
2024-12-03 15:47:41 +0300

    FEAT: adapt Faq Page

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -6,11 +6,11 @@ import Title from "@/components/title";
 
 export default function FAQPage() {
   return (
-    <section className=" flex flex-col text-center gap-5">
+    <section className="flex flex-col text-center gap-5">
       <Title pageTitle="FAQ" />
       <motion.div
         animate={{ opacity: 1 }}
-        className="flex w-3/5 text-start mt-2 text-lg font-extralight dark:font-thin z-50"
+        className="flex w-full text-start mt-2 text-lg font-extralight dark:font-thin z-10"
         initial={{ opacity: 0 }}
         transition={{ duration: 0.5, ease: "easeOut", delay: 0.4 }}
       >
